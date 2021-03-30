b28e076f360622afbb939e19cf23a58bec6d3d58
shubham.rai@tu-dresden.de <shubham.rai@tu-dresden.de>
2021-03-30 15:44:37 +0200

    adding crypto benchmark

--- experiments/mapping_experiment_xmg_cr.cpp
+++ experiments/mapping_experiment_xmg_cr.cpp
@@ -189,8 +189,19 @@ Ntk ntk_optimization( Ntk const& ntk )
   return des;
 }
 
-void tech_map()
+void tech_map( std::string aig_or_klut, const uint32_t& cut_size, bool delay_round, bool req_time)
 {
+    std::string filename = "crypto";
+    filename = filename + aig_or_klut + std::to_string(cut_size) + (delay_round == 0 ? "_false" : "_true") + (req_time == 0 ? "_def": "_max") + ".txt" ;
+    //std::cout << "log file" << filename;
+    std::ofstream outs;
+    outs.open(filename.c_str());
+
+    outs << "aig(0) or klut(1)   "      << aig_or_klut << std::endl;
+    outs << "cut size = "               << cut_size    << std::endl;
+    outs << "delay round (0/1)=  "      << (delay_round ? "true" : "false") << std::endl;
+    outs << "required time (def/max)= " << (req_time ? "true" : "false")  << std::endl;
+
     experiments::experiment<std::string, std::string, std::string>
          exp2( "RFET_area", "benchmark", "sd_rat", "sd_rat'");
 
@@ -210,10 +221,10 @@ void tech_map()
   //    return;
   //}
 
-  for ( auto const& g : gates1 )
-  {
-    std::cout << g.name << std::endl;
-  }
+  //for ( auto const& g : gates1 )
+  //{
+  //  std::cout << g.name << std::endl;
+  //}
   mockturtle::tech_library_params lib_ps;
   lib_ps.very_verbose = false;
   mockturtle::tech_library<5> lib1( gates1, lib_ps );
@@ -274,12 +285,32 @@ void tech_map()
     sop_rebalancing<aig_network> sop_balancing;    
     aig = balancing( aig, {sop_balancing}, sps, &st4 );
 
-    //auto klut = lut_map( aig, 4u );
+    auto klut = lut_map( aig, 4u );
 
+    /* Calling Resynthesis engine */
+    if(aig_or_klut == "aig")
+    {
+        xmg = mockturtle::node_resynthesis<mockturtle::xmg_network>( aig, cached_xmg3_exact );
+        xmg = cleanup_dangling( xmg );
 
-    //imig = mockturtle::node_resynthesis<mockturtle::xmg_network>( klut, npn_resyn );
-    xmg = mockturtle::node_resynthesis<mockturtle::xmg_network>( aig, cached_xmg3_exact );
-    xmg = cleanup_dangling( xmg );
+        aig = mockturtle::node_resynthesis<mockturtle::aig_network>( aig, cached_aig_exact );
+        aig = cleanup_dangling( aig );
+
+        mig = mockturtle::node_resynthesis<mockturtle::mig_network>( aig, mig_npn_resyn );
+        mig = cleanup_dangling( mig );
+    }
+    else
+    {
+        xmg = mockturtle::node_resynthesis<mockturtle::xmg_network>( klut, cached_xmg3_exact );
+        xmg = cleanup_dangling( xmg );
+
+        aig = mockturtle::node_resynthesis<mockturtle::aig_network>( klut, cached_aig_exact );
+        aig = cleanup_dangling( aig );
+
+        mig = mockturtle::node_resynthesis<mockturtle::mig_network>( klut, mig_npn_resyn );
+        mig = cleanup_dangling( mig );
+
+    }
 
     /* measuring sd ratios */
     ps1.reset();
@@ -288,12 +319,6 @@ void tech_map()
     auto size_before = xmg.num_gates();
     double sd_rat = ( double( ps1.actual_maj + ps1.actual_xor3 )/  size_before ) * 100;
     std::string sd_before = fmt::format( "{}/{} = {}", ( ps1.actual_maj + ps1.actual_xor3 ),  size_before, sd_rat );
-
-    //aig = mockturtle::node_resynthesis<mockturtle::aig_network>( klut, cached_aig_exact );
-    //aig = cleanup_dangling( aig );
-
-    mig = mockturtle::node_resynthesis<mockturtle::mig_network>( aig, mig_npn_resyn );
-    mig = cleanup_dangling( mig );
 
     aig = ntk_optimization<mockturtle::aig_network> ( aig );
     mig = ntk_optimization<mockturtle::mig_network> ( mig );
@@ -314,21 +339,26 @@ void tech_map()
     mockturtle::depth_view xmg_d{ xmg };
     mockturtle::depth_view mig_d{ mig };
     mockturtle::depth_view aig_d{ aig };
-    printf( "###################################################\n" );
-    printf( "[i] AIG: n = %d   depth = %d\n",
-            mig.size(), mig_d.depth() );
-    printf( "[i] MIG: n = %d   depth = %d\n",
-            mig.size(), mig_d.depth() );
-    printf( "[i] XMG: n = %d   depth = %d\n",
-            xmg.size(), xmg_d.depth() );
-    fflush( stdout );
+    //printf( "###################################################\n" );
+    //printf( "[i] AIG: n = %d   depth = %d\n",
+    //        mig.size(), mig_d.depth() );
+    //printf( "[i] MIG: n = %d   depth = %d\n",
+    //        mig.size(), mig_d.depth() );
+    //printf( "[i] XMG: n = %d   depth = %d\n",
+    //        xmg.size(), xmg_d.depth() );
+    //fflush( stdout );
 
     mockturtle::map_params ps;
-    ps.cut_enumeration_ps.cut_size = 5;
+    ps.cut_enumeration_ps.cut_size = cut_size;
     ps.cut_enumeration_ps.cut_limit = 25;
     ps.verbose = true;
-    ps.skip_delay_round = false;
-    ps.required_time = std::numeric_limits<float>::max();
+
+    if (delay_round)
+        ps.skip_delay_round = true;
+    else
+        ps.skip_delay_round = false;
+    if (req_time)
+        ps.required_time = std::numeric_limits<float>::max();
    
     mockturtle::map_stats aig_mst, mig_mst, xmg_mst;
 
@@ -351,14 +381,22 @@ void tech_map()
     exp2.save();
     exp2.table();
   }
-  exp.save();
-  exp.table();
-  exp2.save();
-  exp2.table();
+  outs.close();
+  outs.open(filename.c_str(), std::ios::app);
+  exp.save("1");
+  exp.table("1", outs); 
+  exp2.save("1");
+  exp2.table("1",outs); 
+  outs.close();
 }
 
-int main()
+int main( int argc, char* argv[])
 {
-  tech_map();
+  std::cout << "aig(0) or klut(1)   "      << argv[1] << std::endl;
+  std::cout << "cut size = "               << argv[2] << std::endl;
+  std::cout << "delay round (0/1)=  "      << argv[3] << std::endl;
+  std::cout << "required time (def/max)= " << argv[4] << std::endl;
+
+  tech_map( argv[1], std::atoi(argv[2]), std::atoi(argv[3]), std::atoi(argv[4]));
   return 0;
 }
