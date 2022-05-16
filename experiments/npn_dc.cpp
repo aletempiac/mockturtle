0fd93504a444f625fc0699fd82bbb51d063f8293
aletempiac <alessandro.tempia@gmail.com>
2022-05-16 18:17:00 +0200

    Experimenting with dont cares

--- experiments/npn_dc.cpp
+++ experiments/npn_dc.cpp
@@ -46,16 +46,6 @@ int main()
 
   constexpr uint32_t NInputs = 4;
 
-  /* NPN classes */
-  std::unordered_set<static_truth_table<NInputs>, hash<static_truth_table<NInputs>>> classes;
-  static_truth_table<NInputs> tt;
-  do
-  {
-    const auto res = exact_npn_canonization( tt );
-    classes.insert( std::get<0>( res ) );
-    next_inplace( tt );
-  } while ( !is_const0( tt ) );
-
   /* library to map to XAGs */
   xag_npn_resynthesis<xag_network, xag_network, xag_npn_db_kind::xag_complete> resyn;
   exact_library_params ps;
@@ -63,82 +53,22 @@ int main()
   ps.use_dont_cares = true;
   exact_library<xag_network, decltype( resyn )> exact_lib( resyn, ps );
 
-  /* save the size for each NPN class */
-  std::unordered_map<static_truth_table<NInputs>, uint32_t, hash<static_truth_table<NInputs>>> class_sizes;
-  for ( auto const& entry : classes )
-  {
-    // print_binary( entry );
-    const auto res = exact_lib.get_supergates( entry );
-    const unsigned numgates = res ? static_cast<unsigned>( res->front().area ) : 0;
-    // std::cout << fmt::format( "\t {}\n", numgates );
-    class_sizes.insert( {entry, numgates} );
-  }
+  /* create a function */
+  static_truth_table<NInputs> tt, dc;
+  create_from_hex_string( tt, "8000" );
+  create_from_hex_string( dc, "8000" );
 
-  /* study don't cares */
-  for ( auto entry1 = classes.begin(); entry1 != classes.end(); ++entry1 )
-  {
-    // print_binary( *entry1 );
+  /* get the NPN representative of tt */
+  auto [tt_npn, phase, perm] = exact_npn_canonization( tt );
 
-    std::unordered_map<static_truth_table<NInputs>, uint32_t, hash<static_truth_table<NInputs>>> dc_sets;
-    for ( auto entry2 = classes.begin(); entry2 != classes.end();  ++entry2 )
-    {
-      uint32_t size = class_sizes[*entry2];
+  /* permute the DC */
+  auto dc_npn = create_from_npn_config( std::make_tuple( dc, phase, perm ) );
 
-      /* evaluate DC only for size improvement */
-      if ( size >= class_sizes[*entry1] )
-        continue;
+  auto const standard_match = exact_lib.get_supergates( tt_npn );
+  auto const dc_match = exact_lib.get_supergates( tt_npn, dc_npn, phase, perm );
 
-      exact_npn_canonization( *entry2, [&]( auto const& tt ) {
-        /* extract the DC set */
-        const auto dc = *entry1 ^ tt;
-
-        // if ( count_ones( dc ) > 3 )
-        //   return;
-
-        /* check existance */
-        if ( auto const& p = dc_sets.find( dc ); p != dc_sets.end() )
-        {
-          if ( size < std::get<1>( *p ) )
-            dc_sets[dc] = size;
-
-          return;
-        }
-
-        /* check dominance */
-        auto it = dc_sets.begin();
-        while ( it != dc_sets.end() )
-        {
-          auto const& dc_set_tt = std::get<0>( *it );
-          auto const& and_tt = dc_set_tt & dc;
-
-          if ( dc_set_tt == and_tt && std::get<1>( *it ) <= size )
-          {
-            return;
-          }
-          else if ( dc == and_tt && size <= std::get<1>( *it ) )
-          {
-            it = dc_sets.erase( it );
-          }
-          else
-          {
-            ++it;
-          }
-        }
-
-        /* insert in the dc_sets */
-        dc_sets[dc] = size;
-      } );
-    }
-
-    uint32_t max_gain = 0;
-    float avg_gain = 0;
-    for ( auto const& dc : dc_sets )
-    {
-      max_gain = std::max( max_gain, class_sizes[*entry1] - std::get<1>( dc ) );
-      avg_gain += class_sizes[*entry1] - std::get<1>( dc );
-    }
-    // std::cout << fmt::format( "\t {:>5d}\t {:>5d}\t {:>5.3f}\n", dc_sets.size(), max_gain, avg_gain / dc_sets.size() );
-  }
+  std::cout << fmt::format( "Standard match size {}\n", (unsigned) standard_match->front().area );
+  std::cout << fmt::format( "DC       match size {}\n", (unsigned) dc_match->front().area );
 
   return 0;
 }
