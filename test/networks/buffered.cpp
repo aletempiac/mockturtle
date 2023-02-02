3ec64d4a8c467bf2d1e6dee5091b9a977fe8b964
aletempiac <alessandro.tempia@gmail.com>
2023-02-02 12:01:17 +0100

    Removing AQFP tests

--- test/networks/buffered.cpp
+++ test/networks/buffered.cpp
@@ -72,18 +72,15 @@ TEST_CASE( "buffered networks", "[buffered]" )
 {
   test_buffered_network<buffered_aig_network>();
   test_buffered_network<buffered_mig_network>();
-  test_buffered_network<buffered_aqfp_network>();
 }
 
 TEST_CASE( "is_buffered_network_type", "[buffered]" )
 {
   CHECK( is_buffered_network_type_v<buffered_aig_network> );
   CHECK( is_buffered_network_type_v<buffered_mig_network> );
-  CHECK( is_buffered_network_type_v<buffered_aqfp_network> );
   CHECK( is_buffered_network_type_v<buffered_crossed_klut_network> );
 
   CHECK( !is_buffered_network_type_v<aig_network> );
   CHECK( !is_buffered_network_type_v<mig_network> );
   CHECK( !is_buffered_network_type_v<klut_network> );
-  CHECK( !is_buffered_network_type_v<aqfp_network> );
 }
